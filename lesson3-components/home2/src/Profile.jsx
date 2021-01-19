95b4d20ca3be112238c879f8ad9552d49a024778
Alex Klyde <alexeykalayda@gmail.com>
2021-01-19 21:28:45 +0200

    les3-home2

---
+++ lesson3-components/home2/src/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import moment from 'moment';
+
+const getCurrentAge = date => moment(date).format('DD MMM YY');
+
+const Profile = (props) => {
+  return (
+    <div className="profile">
+      <div className="profile__name">
+        {`${props.firstName} ${props.lastName} `}
+      </div>
+      <div className="profile__birth">
+        {`Was born ${getCurrentAge(props.birthDate)} in ${props.birthPlace}`}
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
+
+
+
