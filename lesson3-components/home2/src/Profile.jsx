987bc1740415eac3f4a9793edd126997d93c84ea
Alex Klyde <alexeykalayda@gmail.com>
2021-01-19 21:34:58 +0200

    les3-home2

--- lesson3-components/home2/src/Profile.jsx
+++ lesson3-components/home2/src/Profile.jsx
@@ -5,14 +5,14 @@ const getCurrentAge = date => moment(date).format('DD MMM YY');
 
 const Profile = (props) => {
   return (
-    <div className="profile">
+    <>
       <div className="profile__name">
         {`${props.firstName} ${props.lastName}`}
       </div>
       <div className="profile__birth">
         {`Was born ${getCurrentAge(props.birthDate)} in ${props.birthPlace}`}
       </div>
-    </div>
+    </>
   )
 }
 
