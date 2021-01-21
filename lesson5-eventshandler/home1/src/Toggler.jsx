d9880d3850bb3ffb71bfdc5efc02830ad4ed7060
Alex Klyde <alexeykalayda@gmail.com>
2021-01-21 10:25:15 +0200

    les5-home1

---
+++ lesson5-eventshandler/home1/src/Toggler.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Toggler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+   
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button className="toggler" onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'On' : 'Off'}
+      </button>
+    );
+  }
+}
+
+export default Toggler;
+
