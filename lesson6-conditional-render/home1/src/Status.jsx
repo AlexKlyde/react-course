196e2a49b670d4786d458cf4e00bc1be8a029eb0
Alex Klyde <alexeykalayda@gmail.com>
2021-01-22 21:23:03 +0200

    les6-home1

---
+++ lesson6-conditional-render/home1/src/Status.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Online from './Online';
+import Offline from './Offline';
+
+const Status = props => {
+  return (
+    <>
+        {props.isOnline ? <Online /> : <Offline />}
+    </>
+  );
+};
+
+export default Status;
+
+
