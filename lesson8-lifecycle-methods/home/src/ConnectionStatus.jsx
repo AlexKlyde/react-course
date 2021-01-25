1a56db3a9a2f59790c04a969ef0c1f6d1fe8deed
Alex Klyde <alexeykalayda@gmail.com>
2021-01-25 11:39:09 +0200

    les8-home

--- lesson8-lifecycle-methods/home/src/ConnectionStatus.jsx
+++ lesson8-lifecycle-methods/home/src/ConnectionStatus.jsx
@@ -2,26 +2,34 @@ import React, { Component } from 'react';
 
 class ConnectionStatus extends Component {
   state = {
-    online: true,
+    status: true,
   }
 
    componentDidMount() {
-    window.addEventListener('online', this.handleNetworkChange);
-    window.addEventListener('offline', this.handleNetworkChange);
+    window.addEventListener('online', this.onLine);
+    window.addEventListener('offline', this.offLine);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('online', this.handleNetworkChange);
-    window.removeEventListener('offline', this.handleNetworkChange);
+    window.removeEventListener('online', this.onLine);
+    window.removeEventListener('offline', this.offLine);
   }
 
-  handleNetworkChange = () => {
-    this.setState({ online: window.navigator.onLine });
+  onLine = () => {
+    this.setState({
+      status: true,
+    });
+  }
+
+  offLine = () => {
+    this.setState({
+      status: false,
+    });
   }
 
   render() {
     return (
-        this.state.online
+        this.state.status
         ? <div className="status">online</div>
         : <div className="status status_offline">offline</div>
     );
