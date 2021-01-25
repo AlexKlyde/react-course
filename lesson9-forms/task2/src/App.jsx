f2d7b4ccc0962ae2061821dc9212d6bef6a71a98
Alex Klyde <alexeykalayda@gmail.com>
2021-01-25 14:17:50 +0200

    les9-task2

---
+++ lesson9-forms/task2/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserForm from './UserForm';
+
+const App = () => {
+  return <div>Hello, React!</div>;
+};
+
+export default App;
--- lesson9-forms/task2/src/UserForm.jsx
+++ lesson9-forms/task2/src/UserForm.jsx
@@ -69,8 +69,8 @@ class UserForm extends Component {
           >
             <option value="london">London</option>
             <option value="new-york">New York</option>
-            <option value="coconut">Sidney</option>
-            <option value="mango">Berlin</option>
+            <option value="sidney">Sidney</option>
+            <option value="berlin">Berlin</option>
           </select>
         </div>
 
