6cde188282bf44a6d65f0e0caeb779dccbd14f92
Alex Klyde <alexeykalayda@gmail.com>
2021-01-25 19:09:10 +0200

    les9-home1

---
+++ lesson9-forms/home1/src/UsersList.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import User from './User';
+import Filter from './Filter';
+
+class UsersList extends Component {
+  state = { value: '' };
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  render() {
+    const usersList = this.props.users;
+    const updatedList = usersList.some(
+      element => element.name.toLowerCase() === this.state.value.toLowerCase(),
+    )
+      ? usersList.filter(
+          user => user.name.toLowerCase() === this.state.value.toLowerCase(),
+        )
+      : usersList;
+
+    return (
+      <div>
+        <Filter
+          filterText={this.state.value}
+          count={usersList.length}
+          onChange={this.handleChange}
+        />
+        <ul className="users">
+          {updatedList.map(user => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UsersList;
