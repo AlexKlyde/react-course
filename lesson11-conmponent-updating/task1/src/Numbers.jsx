1bd16cdb9b2d2dc9f31cfe93c64770860b7f4c14
Alex Klyde <alexeykalayda@gmail.com>
2021-01-27 14:59:15 +0200

    les11-task1 proptypes added

--- lesson11-conmponent-updating/task1/src/Numbers.jsx
+++ lesson11-conmponent-updating/task1/src/Numbers.jsx
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
 class Numbers extends PureComponent {
 
@@ -20,5 +21,14 @@ class Numbers extends PureComponent {
     );
   }
 }
+
+Numbers.propTypes = {
+  title: PropTypes.string,
+  number: PropTypes.number.isRequired
+}
+
+Numbers.defaultProps = {
+  title: 'Some number',
+};
 
 export default Numbers;
