093e894d41905e512abf8852abdd14980f1bf7b0
Alex Klyde <alexeykalayda@gmail.com>
2021-01-29 21:13:15 +0200

    les13-home

---
+++ lesson13-react-router/home-addition/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Users from './Users';
+import Home from './Home';
+
+const App = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/users" component={Users}>
+        </Route>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
